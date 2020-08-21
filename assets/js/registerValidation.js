6e69f281e0ee82bff52e08f0658cadb8c6462658
Luteya Coulston <thatcoul@gmail.com>
2020-08-21 19:00:16 +0300

    register.php javascript validation added

--- assets/js/registerValidation.js
+++ assets/js/registerValidation.js
@@ -1,17 +1,63 @@
+let form = document.querySelector("#register_form");
 let firstName = document.querySelector("#fName");
 let email = document.querySelector("#email");
 let username = document.querySelector("#username");
 let password = document.querySelector("#password");
 let passwordConfirm = document.querySelector("#passwordConfirm");
 let register = document.querySelector("#register");
+let errors = [];
 
-function allLetter(inputtxt) {
-  var letters = /^[A-Za-z]+$/;
-  if (inputtxt.value.match(letters)) {
-    alert("Your name have accepted : you can try another");
-    return true;
-  } else {
-    alert("Please input alphabet characters only");
-    return false;
-  }
+function invalidAlert(errorFieldSelector, errorText) {
+  let selector = document.querySelector(errorFieldSelector);
+  selector.innerText = errorText;
 }
+
+form.addEventListener("submit", function (event) {
+  if (
+    firstName.value == "" &&
+    email.value == "" &&
+    username.value == "" &&
+    password.value == "" &&
+    passwordConfirm.value == ""
+  ) {
+    alert("Fill all fields");
+    firstName.focus();
+    event.preventDefault();
+    return;
+  }
+  if (firstName.value.trim() === "") {
+    errors.push(true);
+    firstName.focus();
+    invalidAlert("#fNameError", "First name not filled");
+    event.preventDefault();
+    return;
+  }
+  if (email.value.trim() === "") {
+    errors.push(true);
+    email.focus();
+    invalidAlert("#emailError", "Email not filled");
+    event.preventDefault();
+    return;
+  }
+  if (username.value.trim() === "") {
+    errors.push(true);
+    username.focus();
+    invalidAlert("#usernameError", "Username not filled");
+    event.preventDefault();
+    return;
+  }
+  if (password.value.trim() === "") {
+    errors.push(true);
+    password.focus();
+    invalidAlert("#passwordError", "Password not filled");
+    event.preventDefault();
+    return;
+  }
+  if (passwordConfirm.value.trim() === "") {
+    errors.push(true);
+    passwordConfirm.focus();
+    invalidAlert("#confirmError", "You did not confirm password");
+    event.preventDefault();
+    return;
+  }
+});
--- templates/register.html.php
+++ templates/register.html.php
@@ -1,24 +1,24 @@
 <form action="" method="POST" id="register_form">
     <label for="First name">First name: </label> <br>
     <input type="text" name="first_name" id="fName" value=<?= $_POST['first_name'] ?? '' ?>>
-    <span class="error"> <?php echo $nameErr; ?></span>
+    <span class="error" id="fNameError"> <?php echo $nameErr; ?></span>
     <br>
     <label for="email">Email: </label> <br>
     <input type="text" name="email" id="email" value=<?= $_POST['email'] ?? '' ?>>
-    <span class="error"> <?php echo $emailErr; ?></span>
+    <span class="error" id="emailError"> <?php echo $emailErr; ?></span>
     <br>
     <label for="username">Username</label> <br>
     <input type="text" name="username" id="username" value=<?= $_POST['username'] ?? '' ?>>
-    <span class="error"> <?php echo $usernameErr; ?></span>
+    <span class="error" id="usernameError"> <?php echo $usernameErr; ?></span>
     <br>
     <label for="password">Password: </label> <br>
     <input type="password" id="password" name="password">
-    <span class="error"> <?php echo $passwordErr; ?></span>
+    <span class="error" id="passwordError"> <?php echo $passwordErr; ?></span>
     <br>
     <label for="Confirm password">Confirm password: </label> <br>
     <input type="password" id="passwordConfirm" name="password_confirm">
-    <span class="error"> <?php echo $confirmErr; ?></span>
+    <span class="error" id="confirmError"> <?php echo $confirmErr; ?></span>
     <br>
     <input type="submit" name="register" id="register" value="Register"> <br>
 </form>
-<script src="../assets/js/registerValidation.js"></script>
+<script src='../assets/js/registerValidation.js'></script>
